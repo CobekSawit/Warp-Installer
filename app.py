36df27a1df45459d9c4da6909ecc7a7e2140d500
Cobek Sawit <114243000+CobekSawit@users.noreply.github.com>
2023-01-22 14:25:25 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+# Python code obfuscated by www.development-tools.net 
+ 
+
+import base64, codecs
+magic = 'ZnJvbSBraW1pbi5jb3JlIGltcG9ydCBDb3JlDQoNCiNVcmwgRmlsZSBHaXRodWINCmdpdGh1Yl9maWxlID0gImh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9Db2Jla1Nhd2l0L2lwa3UvbWFpbiINCnNpbiA9IENvcmUoKQ0KDQojVXNlcm5hbWUgR2l0aHViDQpzaW4udXNlcm5hbWUgPSAnQ29iZWtTYXdpdCcNCg0KI1Rva2VuIERhcmkgR2l0aHViDQpzaW4udG9rZW4gPSAnZ2l0aHViX3'
+love = 'OuqS8kZHRmFSEZG0RjFH9iG3ycLIyCo0cCKmI0L1AgH3VlHTt0oxqMH3WQJxARZ0u4HUEMLxIyqRSxGH1Yq0kVJaI1GaEDGxEBAIyMIIqkEwSKHSN0Wj0XQDbwH2yhqTS4VSIhqUIeVR1yoKOypz9fMJttHUIvoTywVRyDQDccpPN9VUAcov5UMKEWHPtcQDbAPaOlnJ50XPp9Wlb1ZPxAPaOlnJ50XTLaGJIhM2IwMJftFIOpoyMypaAco24tBvNkYwOpoxAlMJS0MJDtDax6VRAiLzIeVSAuq2y0WlxAPaOl'
+god = 'aW50KCc9Jyo1MCkNCnByaW50KGYiTXkgUHVibGljIElQIDoge2lwfSIpDQoNCiNTaW50YXggVW50dWsgTWVtcGVyb2xlaCBEYXRhIGRhcmkgR2l0aHViDQpkYiA9IHNpbi5HZXRHaXRodWIoJ2h0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9Db2Jla1Nhd2l0L2lwa3UvbWFpbi9kYWZ0YXInKQ0KDQppZiBub3QgZGIgaXMgTm9uZSBhbmQgaXAgaW4gZGIuc3BsaXQoJ1xuJyk6DQoJcHJpbn'
+destiny = 'DbW0yDVTEcVRy6nJ5eLJ4aXD0XPJygpT9lqPOipj0XPJ9mYaA5p3EyoFtvLzSmnPNiMKEwY3ulLKxiL29vMJgmLKqcqPOgMJ51VvxAPzIfp2H6QDbWpUWcoaDbWm0aXwHjXD0XPKOlnJ50XPqWHPOHnJEunlOxnFOWrzyhn2ShWlxAPtyjpzyhqPuzW1AcoTSbn2ShVTEuMaEupzguovOWHPOOozEuKT5poxAiLzIeVSAuq2y0KT50Yz1yY2AiLzIep2S3nKEiMzMcL2yuoPpcQDbWpUWcoaDbWm0aXwHjXD=='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
